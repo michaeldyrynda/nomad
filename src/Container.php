70600f74a06a05115a9cd77201cedc9262d7f583
Michael Dyrynda <michael@dyrynda.com.au>
2018-01-12 09:24:38 +1030

    ltrim basePath to avoid extraneous slashses, add whitespace

--- src/Container.php
+++ src/Container.php
@@ -26,6 +26,8 @@ class Container extends BaseContainer implements LaravelApplication
 
     public function basePath($path = '')
     {
+        $path = ltrim($path, '/');
+
         return BASE_PATH.($path ? DIRECTORY_SEPARATOR.$path : $path);
     }
 
@@ -109,7 +111,9 @@ class Container extends BaseContainer implements LaravelApplication
         if (! is_null($this->namespace)) {
             return $this->namespace;
         }
+
         $composer = json_decode(file_get_contents(base_path('composer.json')), true);
+
         foreach ((array) data_get($composer, 'autoload.psr-4') as $namespace => $path) {
             foreach ((array) $path as $pathChoice) {
                 if (realpath(app_path()) == realpath(base_path().'/'.$pathChoice)) {
@@ -117,6 +121,7 @@ class Container extends BaseContainer implements LaravelApplication
                 }
             }
         }
+
         throw new RuntimeException('Unable to detect application namespace.');
     }
 
